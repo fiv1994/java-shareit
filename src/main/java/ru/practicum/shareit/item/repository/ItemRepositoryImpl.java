6e379b679bfaa56e40f3ca8e5f2ba2e34da03557
Иван Филиппов <filippov@power.corp>
2025-02-18 20:17:56 +0300

    Корректировка check-style

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepositoryImpl.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
 @Slf4j
